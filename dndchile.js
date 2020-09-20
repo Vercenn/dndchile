778a04ef58821b4613f0a06805e149ca98d22bb2
Vercenn <33332434+Vercenn@users.noreply.github.com>
2020-09-20 00:34:58 -0300

    Add files via upload

---
+++ dndchile.js
@@ -0,0 +1,26 @@
+{
+	"_meta": {
+		"sources": [
+			{
+				"json": "DNDCL",
+				"abbreviation": "DNDCL",
+				"full": "Valores Custom de D&D Chile",
+				"authors": [
+					"Vercenn"
+				],
+				"version": "0.1.1",
+				"color": "b1b1b1",
+				"url": "f.f/f.html",
+				"targetSchema": "1.0.0"
+			}
+		],
+		"dateAdded": 1591318518,
+		"dateLastModified": 1596801281
+	},
+	"raceFluff": [
+		{
+			"name": "Tokens de Tesoro",
+			"source": "D&D Chile",
+			"entries": [
+				"TOKEN T1\t\t\t\t\t3\nTOKEN T2\t\t\t\t\t20\nTOKEN T3\t\t\t\t\t11",
+				{
